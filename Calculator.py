d9880e56e9ac2b4abe58fa3fae6670c1b68edc2c
JadenCantCode <65595516+JadenCantCode@users.noreply.github.com>
2024-08-08 18:07:22 +0800

    Add files via upload
    Starting new project

---
+++ Calculator.py
@@ -0,0 +1,24 @@
+import math
+import random
+import time
+
+while True:
+    print("Welcome to \"Calculator\"")
+    time.sleep(3)
+    print("What would you like to do?")
+    time.sleep(2)
+    print("""1: Use Calculator
+    2: Exit""")
+
+    while True: 
+        start = input("Choice: ")
+        if start == "1":
+            print("You have chosen to use the calculator...")
+            time.sleep(2)
+            print("Correct? (Y/N)")
+            time.sleep(2)
+            calc = input("Choice: ")
+            if calc == ("N", "n"):
+                print("Returning to choice...")
+                time.sleep(2)
+                break
